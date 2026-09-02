fe38279bc731fe82c6c6ee7ea7f3507ab8e55290
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-02 10:58:41 +0100

    feat: add global exception handler

--- src/main/java/com/logitrack/notification_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/logitrack/notification_service/exception/GlobalExceptionHandler.java
@@ -2,14 +2,17 @@ package com.logitrack.notification_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResponseStatusException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
         return ResponseEntity
                 .status(ex.getStatusCode())
